1286327a626cd92e6f82178211c78ffc8028547a
Anikald <t1kend.offical@gmail.com>
2021-11-03 01:06:47 +1000

    Lesson 3: Add news model, migration, factory and seeder 3.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        (new NewsSeeder)->run();
+        $this->call([
+            NewsSeeder::class
+        ]);
     }
 }
