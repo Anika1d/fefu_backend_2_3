42ce25457d31f423bd626b52c01c4f8a4c91c298
Anika1d <71558328+Anika1d@users.noreply.github.com>
2021-11-25 12:39:07 +1000

    Lesson 4: Add news routes, controller and views (#3)
    * Lesson 4: Add news routes, controller and views
    
    * Lesson 4: Add news routes, controller and views 2.0
    
    Co-authored-by: Anikald <t1kend.offical@gmail.com>

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -7,14 +7,9 @@ use Illuminate\Database\Seeder;
 
 class NewsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         News::query()->delete();
-        News::factory(random_int(15,25))->create();
+        News::factory(random_int(20,30))->create();
     }
 }
