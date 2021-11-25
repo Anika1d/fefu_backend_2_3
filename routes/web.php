6bf83cc8b8c80b43f6fae08ba481b61f16354bf6
Anika1d <71558328+Anika1d@users.noreply.github.com>
2021-11-26 00:32:00 +1000

    Lesson 5: Add appeal form (#4)
    Co-authored-by: Anikald <t1kend.offical@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,11 @@
 
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AppealController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 Route::get('/news/', [NewsController::class, 'getList'])->name('news_list_rol');
 Route::get('/news/{slug}', [NewsController::class, 'getDetails'])->name('news_rol');
-
+Route::match(['get','post'],'/appeal', AppealController::class)->name('appeal_rol');
+   
