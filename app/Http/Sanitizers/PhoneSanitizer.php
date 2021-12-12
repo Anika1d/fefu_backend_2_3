3f391170fe612ae4d934f714aedd617c7c51dd7c
Anikald <t1kend.offical@gmail.com>
2021-12-12 16:06:31 +1000

    Update PhoneSanitizer.php
    я не знаю как так получилось. откатил  и починил =(

--- app/Http/Sanitizers/PhoneSanitizer.php
+++ app/Http/Sanitizers/PhoneSanitizer.php
@@ -4,8 +4,12 @@ namespace App\Http\Sanitizers;
 
 class PhoneSanitizer
 {
-    public static function sanitize(string $value): string
+    public static function sanitize(string $value): ?string
     {
+        if ($value === null) {
+            return null;
+        }
+
         return preg_replace('/^8/', '7', preg_replace('/\D+/', '',$value));
     }
 }
