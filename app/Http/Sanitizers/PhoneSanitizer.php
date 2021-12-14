8732f83a988247e855a87328b0cb32d95c106347
Anika1d <71558328+Anika1d@users.noreply.github.com>
2021-12-14 14:12:28 +1000

    Lesson 7: Add redirecting middleware

--- app/Http/Sanitizers/PhoneSanitizer.php
+++ app/Http/Sanitizers/PhoneSanitizer.php
@@ -4,8 +4,10 @@ namespace App\Http\Sanitizers;
 
 class PhoneSanitizer
 {
-    public static function sanitize(string $value): string
+    public static function sanitize(?string $value): ?string
     {
-        return preg_replace('/^8/', '7', preg_replace('/\D+/', '',$value));
+        if ($value === null)
+            return null;
+        return preg_replace('/^8/', '7', preg_replace('/\D+/', '', $value));
     }
 }
